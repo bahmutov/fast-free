6220b9c97abf2108d6519a6ec947b7da73920c60
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-14 22:15:43 -0500

    use cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,15 @@
 const { defineConfig } = require('cypress')
-
+// https://github.com/bahmutov/cypress-split
+const cypressSplit = require('cypress-split')
 module.exports = defineConfig({
   e2e: {
     // baseUrl, etc
     supportFile: false,
     fixturesFolder: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // and load any plugins that require the Node environment
+      cypressSplit(on, config)
+      // IMPORTANT: return the config object
+      return config
     },
   },
 })
